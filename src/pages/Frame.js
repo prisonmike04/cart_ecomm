bb493f3ff8a6fb8b4bc9ada29297c748a20b5195
prisonmike04 <saayushi.829@gmail.com>
2023-12-07 17:01:19 +0530

    cart

---
+++ src/pages/Frame.js
@@ -0,0 +1,152 @@
+import "./Frame.css";
+
+const Frame = () => {
+  return (
+    <div className="frame-parent">
+      <div className="frame">
+        <div className="macbook-air-1">
+          <div className="ipad-mini-83-1">
+            <div className="frame1">
+              <div className="frame2">
+                <div className="frame3">
+                  <img className="arrow-icon" alt="" src="/arrow@2x.png" />
+                  <div className="shopping-continue">Shopping Continue</div>
+                </div>
+                <div className="frame-child" />
+              </div>
+              <div className="frame4">
+                <div className="frame5">
+                  <div className="shopping-cart">Shopping cart</div>
+                  <div className="you-have-3">You have 3 item in your cart</div>
+                </div>
+                <div className="frame6">
+                  <div className="frame7">
+                    <div className="div">1</div>
+                    <div className="boat-headphones">boat headphones</div>
+                    <div className="noise-cancellation">Noise cancellation</div>
+                    <img
+                      className="frame-item"
+                      alt=""
+                      src="/rectangle-19@2x.png"
+                    />
+                    <img className="frame-inner" alt="" src="/group-4.svg" />
+                    <div className="div1">$681</div>
+                    <img
+                      className="trash-can-icon"
+                      alt=""
+                      src="/trash-can@2x.png"
+                    />
+                  </div>
+                  <div className="frame7">
+                    <div className="div">1</div>
+                    <div className="boat-headphones">Mac Book Pro</div>
+                    <div className="noise-cancellation">With free Air pods</div>
+                    <img
+                      className="frame-item"
+                      alt=""
+                      src="/rectangle-21@2x.png"
+                    />
+                    <img className="frame-inner" alt="" src="/group-4.svg" />
+                    <div className="div1">$681</div>
+                    <img
+                      className="trash-can-icon"
+                      alt=""
+                      src="/trash-can1@2x.png"
+                    />
+                  </div>
+                  <div className="frame7">
+                    <div className="div">1</div>
+                    <div className="boat-headphones">Iphone 14 pro max</div>
+                    <div className="noise-cancellation">Latest edition</div>
+                    <img
+                      className="frame-item"
+                      alt=""
+                      src="/rectangle-23@2x.png"
+                    />
+                    <img className="frame-inner" alt="" src="/group-4.svg" />
+                    <div className="div1">$681</div>
+                    <img
+                      className="trash-can-icon"
+                      alt=""
+                      src="/trash-can2@2x.png"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="frame10">
+              <div className="name-on-card">Name on card</div>
+              <div className="card-type">Card type</div>
+              <div className="card-number">Card Number</div>
+              <div className="line-div" />
+              <div className="frame11">
+                <div className="card-details">Card Details</div>
+                <img
+                  className="frame-child3"
+                  alt=""
+                  src="/rectangle-3@2x.png"
+                />
+              </div>
+              <div className="frame12">
+                <img
+                  className="frame-child4"
+                  alt=""
+                  src="/rectangle-9@2x.png"
+                />
+                <img
+                  className="frame-child5"
+                  alt=""
+                  src="/rectangle-10@2x.png"
+                />
+                <img
+                  className="frame-child6"
+                  alt=""
+                  src="/rectangle-11@2x.png"
+                />
+                <div className="rectangle-div" />
+                <b className="see-all">See all</b>
+              </div>
+              <div className="frame13">
+                <div className="name">Name</div>
+              </div>
+              <div className="frame14">
+                <div className="div6">1111 2222 3333 4444</div>
+              </div>
+              <div className="frame15">
+                <div className="expiration-date">Expiration date</div>
+                <div className="expiration-date">CVV</div>
+              </div>
+              <div className="frame16">
+                <div className="frame-child7" />
+                <div className="mmyy">mm/yy</div>
+                <div className="frame-child8" />
+                <div className="div7">123</div>
+              </div>
+              <div className="frame17">
+                <div className="expiration-date">Subtotal</div>
+                <div className="div8">$1,668</div>
+              </div>
+              <div className="frame18">
+                <div className="expiration-date">Shipping</div>
+                <div className="div8">$4</div>
+              </div>
+              <div className="frame19">
+                <div className="expiration-date">Total (Tax incl.)</div>
+                <div className="div8">$1,672</div>
+              </div>
+              <div className="frame20">
+                <div className="frame21">
+                  <div className="expiration-date">$1,672</div>
+                  <div className="checkout">Checkout</div>
+                </div>
+                <img className="right-icon" alt="" src="/right@2x.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
